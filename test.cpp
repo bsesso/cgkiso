1ca891e7e51beed14470f8995b1dca84aae2f021
SESSO BRUNO <s1555648@borage02.coins.tsukuba.ac.jp>
2016-10-04 10:54:44 +0900

    clock

--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <GLUT/glut.h>
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
+#include <math.h>
+//#include <OpenGL/gl.h>
+//#include <OpenGL/glu.h>
 
 using namespace std;
 
@@ -11,9 +12,22 @@ void display() {
 
     glColor3d(0.0, 0.0, 1.0);
     glBegin(GL_LINE_LOOP);
-    glVertex2d(-0.9, -0.8);
-    glVertex2d( 0.9, -0.8);
-    glVertex2d( 0.0,  0.8);
+    
+    for (int i = 0; i < 360; i++) {
+        double x = cos(i * 3.14159 / 180.0);
+        double y = sin(i * 3.14159 / 180.0);
+        glVertex2d(x * 0.6, y * 0.6);
+    }
+    glEnd();
+    
+    glBegin(GL_LINES);
+    
+    for (int i = 0; i < 12; i++) {
+        double x = cos(i * 30 * 3.14159 / 180.0);
+        double y = sin(i * 30 * 3.14159 / 180.0);
+        glVertex2d(x * 0.55, y * 0.55);
+        glVertex2d(x * 0.45, y * 0.45);
+    }
     glEnd();
     
 	glFlush();
@@ -25,6 +39,6 @@ int main(int argc, char *argv[]) {
 	glutCreateWindow(argv[0]);
 	glutDisplayFunc(display);
 	glutMainLoop();	
-	
+	€‹›
 	return 0;
 }
